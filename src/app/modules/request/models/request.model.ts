36f0be033483a34dd19592c329526bcaa901a1b2
Eledi Dyrkaj <edyrkaj@gmail.com>
2018-05-18 15:41:34 +0200

    created model objects for request and transaction part of request response

---
+++ src/app/modules/request/models/request.model.ts
@@ -0,0 +1,12 @@
+export class Request {
+
+  creator: string;
+  currencyContract: any;
+  data: any;
+  payee: any;
+  payer: string; // hash address
+  requestId: string;
+  state: number;
+  subPayees: any;
+  
+}
