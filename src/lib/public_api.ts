d8a7ff2faa3d541139196d051b4cc50b29cb4ccd
Eledi Dyrkaj <edyrkaj@gmail.com>
2018-05-27 10:29:49 +0200

    some improvements on file configurations and package json

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -1,3 +1,2 @@
-// export request-network-service
-export * from './angular-request-network/services/request-network.service';
 export * from './angular-request-network/models/request.model';
+export * from './angular-request-network/services/request-network.service';
